265eeb730ed99c34d02258bc7ed307039127157a
Alex <spykerstar@gmail.com>
2019-05-09 02:26:14 +0200

    updated voice recognition code

--- python/google/VoiceAssistant_MK1/main.py
+++ python/google/VoiceAssistant_MK1/main.py
@@ -1,60 +1,66 @@
-import speech_recognition as sr
-from time import ctime
 import time
-import os
-from gtts import gTTS
+
+import speech_recognition as sr
 
 
-# main functions section
-def speak(audio_string):
-    print(audio_string)
-    tts = gTTS(text=audio_string, lang='en')
-    tts.save("audio.mp3")
-    os.system("mpg123 audio.mp3")
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
 
 
-def record_audio():
-    # Record Audio
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print "Say Something"
-        audio = r.listen(source)
+if __name__ == "__main__":
+    # create recognizer and mic instances
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
 
-        # Speech recognition using Google API
-        data = ""
-        try:
-            # Uses the default API key
-            data = r.recognize_google(audio)
-            print "You said" + data
-        except sr.UnknownValueError:
-            print "Google API could not recognize audio"
-        except sr.RequestError as e:
-            print "Could not get results from Google API service: {0}".format(e)
+    while True:
+        print('Speak!')
+        guess = recognize_speech_from_mic(recognizer, microphone)
 
-    return data
+        # show the user the transcription
+        print("You said: {}".format(guess["transcription"]))
 
-
-def jarvis(data):
-    if "how are you" in data:
-        speak("I am fine")
-
-    if "what time is it" in data:
-        speak(ctime())
-
-    if "where is" in data:
-        data = data.split(" ")
-        location = data[2]
-        speak("Hold on, I will show you where " + location + " is. ")
-        os.system("chromium-browser https://www.google.nl/maps/place/" + location + "/&amp;")
-
-
-# main functions end section
-
-# init section
-time.sleep(2)
-speak("Hi Alex, what can I do for you?")
-while 1:
-    data = record_audio()
-    jarvis(data)
-
-# init section end
+        time.sleep(1)
